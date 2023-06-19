2230656cb05d4f8ecc1bb92d17d050ccbe39d758
4Absolem <89601045+4Absolem@users.noreply.github.com>
2023-06-18 20:40:26 -0400

    Add files via upload

---
+++ Aire.java
@@ -0,0 +1,57 @@
+package org.apache.beam.examples;
+
+public class Aire {
+    private final String nombreEstacion;
+    private final String fechaDatos;
+    private final String temperaturaMinima;
+    private final String horaTemperaturaMinima;
+    private final String temperaturaMaxima;
+    private final String horaTemperaturaMaxima;
+
+    public Aire(String nombreEstacion, String fechaDatos, String temperaturaMinima, String horaTemperaturaMinima,
+                String temperaturaMaxima, String horaTemperaturaMaxima) {
+        this.nombreEstacion = nombreEstacion;
+        this.fechaDatos = fechaDatos;
+        this.temperaturaMinima = temperaturaMinima;
+        this.horaTemperaturaMinima = horaTemperaturaMinima;
+        this.temperaturaMaxima = temperaturaMaxima;
+        this.horaTemperaturaMaxima = horaTemperaturaMaxima;
+    }
+
+    public String getNombreEstacion() {
+        return nombreEstacion;
+    }
+
+    public String getFechaDatos() {
+        return fechaDatos;
+    }
+
+    public String getTemperaturaMinima() {
+        return temperaturaMinima;
+    }
+
+    public String getHoraTemperaturaMinima() {
+        return horaTemperaturaMinima;
+    }
+
+    public String getTemperaturaMaxima() {
+        return temperaturaMaxima;
+    }
+
+    public String getHoraTemperaturaMaxima() {
+        return horaTemperaturaMaxima;
+    }
+    
+    @Override
+    public String toString() {
+        return "Aire{" + "Nombre estacion=" + nombreEstacion + ", Fecha de datos=" + fechaDatos + ", Temperatura minima=" + temperaturaMinima +", Hora de Temperatura Minima=" + horaTemperaturaMinima +", Temperatura Maxima=" + temperaturaMaxima +", Hora de Temperatura Maxima=" + horaTemperaturaMaxima + '}';
+    }
+    
+    public String toCSV()
+    {
+        return "" + nombreEstacion + "," + fechaDatos + "," + temperaturaMinima + "," + horaTemperaturaMinima + "," + temperaturaMaxima + "," + horaTemperaturaMaxima + "";
+    }
+    
+    
+    
+}
